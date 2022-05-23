60b50fbff538e8522a367523d0068a87ff556bc3
Fabio-jr-SM <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-05-23 13:34:17 -0400

    Add files via upload

---
+++ 2º Bimestre/FilleDemostration/FilleDemostration/src/filledemostration/FileDemonstrationTest.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+
+public class FileDemonstrationTest {
+
+    public static void main(String[] args) {
+        FileDemonstration application = new FileDemonstration();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    } 
+}
